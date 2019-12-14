ed7d317b40a2d763a5ce10efadd6770dc6133769
ystechnology <y.soccer75@gmail.com>
2019-12-14 19:07:37 +0900

    メッセージを送信完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to root_path, notice: 'グループを更新しました' #ルートパスではなくそこのらんでのリダイレクトにしなければならない
     else
       render :edit
     end
