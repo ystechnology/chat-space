cde409007fbc82d84754d21f8b811f7fd441bf13
ystechnology <y.soccer75@gmail.com>
2020-01-03 19:04:55 +0900

    自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,66 +1,92 @@
 $(function(){
-    function buildMessage(message){
-    if (message.image){
-      var html = 
-      `<div class="message"> 
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
+  function buildMessage(message){
+  if (message.image){
+    var html = 
+    `<div class="message" data-message-id = ${message.id}> 
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
         </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
+        <div class="upper-message__date">
+          ${message.created_at}
         </div>
-        <img src=${message.image} >
-      </div>`
-    return html
+      </div>
+      <div class="lower-message">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+      <img src=${message.image} >
+    </div>`
+  return html
 
-  } else{
-      var html = 
-      `<div class="message"> 
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
+} else{
+    var html = 
+    `<div class="message" data-message-id = ${message.id}> 
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
         </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
+        <div class="upper-message__date">
+          ${message.created_at}
         </div>
-      </div>`
-      return html;
-    };
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+      </div>
+      <div class="lower-message">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+    </div>`
+    return html;
+  };
+};
+
+$('#new_message').on('submit', function(e){
+  e.preventDefault()
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+  $.ajax({
+    url: url,
+    type: "Post",
+    data: formData,
+    dataType:'json',
+    processData: false,
+    contentType:  false
+  })
+  .done(function(message){
+    var html = buildMessage(message);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    $('form')[0].reset();
+  })
+  .fail(function(){
+    alert('error');
+  });
+  return false;
+});
+
+ function reloadMessages () {
+    last_message_id = $('.message:last').attr("data-message-id");
     $.ajax({
-      url: url,
-      type: "Post",
-      data: formData,
-      dataType:'json',
-      processData: false,
-      contentType:  false
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-    .done(function(message){
-      var html = buildMessage(message);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-      $('form')[0].reset();
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildMessage(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
-    .fail(function(){
+    .fail(function() {
       alert('error');
     });
-    return false;
-  });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 2000);
+}
 });
+
